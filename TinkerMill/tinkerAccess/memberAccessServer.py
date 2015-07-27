2331bdd80d7ea06b8322eefe690aed2400db18b0
Andrew Steffen <asteffen411@gmail.com>
2015-07-27 14:31:27 -0600

    Update memberAccessServer.py
    Just a basic flask implementation. Check with Matt's simpleAccess.

--- TinkerMill/tinkerAccess/memberAccessServer.py
+++ TinkerMill/tinkerAccess/memberAccessServer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
-
+#
+#
+#  This file may be deprecated!!!
+#  TODO:  check with Matt P's simpleServer applications!!!
+#
+# The blow code is a very basic implementation of the flask
+# module.  TODO:  verify which server side implementation will
+# be used.
 from flask import Flask
 app = Flask(__name__)
 
